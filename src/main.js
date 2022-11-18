244d1dabaf061da34cec61c2ca469971005252e3
Vadim Mezentsev <mezentsevvo@gmail.com>
2022-11-18 22:10:14 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const houses = [];
 const { CHAT_ID, BOT_API } = process.env;
 
 const urls = [
-    'https://www.funda.nl/en/huur/amsterdam/0-2000/2+kamers/1-dag/+50km/',
+    'https://www.funda.nl/en/huur/amsterdam/0-2000/2+kamers/1-dag/+30km/',
 ];
 
 const runTask = async () => {
